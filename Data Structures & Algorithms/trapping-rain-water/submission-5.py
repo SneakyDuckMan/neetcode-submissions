0b465487b7f5fa2876883561bb5c815da6579ecd
SneakyDuckMan <ameygupta1997@gmail.com>
2026-05-07 14:39:54 +0530

    Add: trapping-rain-water - submission-5

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        ans = 0
+        max_height_pos = 0
+
+        for i in range(len(height)):
+            if height[i] > height[max_height_pos]:
+                max_height_pos = i 
+
+        start = 0
+        end = len(height) - 1
+
+        while start < end and height[start + 1] > height[start]:
+            start += 1
+
+        while start < end and height[end - 1] > height[end]:
+            end -= 1
+
+        curr_max_height = height[start]
+        for i in range(start + 1, max_height_pos):
+
+            if height[i] > curr_max_height:
+                curr_max_height = height[i]
+                continue
+            
+            ans += curr_max_height - height[i]
+        
+        curr_max_height = height[end]
+        for i in range(end - 1, max_height_pos, -1):
+
+            if height[i] > curr_max_height:
+                curr_max_height = height[i]
+                continue
+            
+            ans += curr_max_height - height[i]
+
+        return ans
